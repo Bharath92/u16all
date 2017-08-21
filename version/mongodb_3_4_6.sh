ded41aec9a1d58ec421891cb996566779d7fb854
Bharath Upadhya <1992bharath@gmail.com>
2017-08-22 01:20:18 +0530

    x

--- version/mongodb_3_4_6.sh
+++ version/mongodb_3_4_6.sh
@@ -2,7 +2,7 @@
 
 echo "================= Installing Mongodb 3.4.6 ==================="
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
-echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+echo "deb [ arch=arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
 sudo apt-get update -qq
 sudo apt-get install -qq -y mongodb-org
 
--- version/postgres_9_6_3.sh
+++ version/postgres_9_6_3.sh
@@ -1,21 +1,21 @@
-#!/bin/bash -e
-
-sudo apt-get install -qq -y wget ca-certificates
-
-echo "================= Installing Postgre 9.6.3 ==================="
-sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-sudo apt-get update -qq
-sudo apt-get install -qq -y postgresql-9.6 postgresql-server-dev-9.6
-sudo apt-get install -qq postgis
-
-# Fix bug https://github.com/docker/docker/issues/783
-# which prevents postgresql from staring when using aufs
-# This workaround proposed in the comment to the issue: https://github.com/docker/docker/issues/783#issuecomment-56013588
-# I've test this on my own builds
-mkdir /etc/ssl/private-copy
-mv /etc/ssl/private/* /etc/ssl/private-copy/
-rm -r /etc/ssl/private
-mv /etc/ssl/private-copy /etc/ssl/private
-chmod -R 0700 /etc/ssl/private
-chown -R postgres /etc/ssl/private
+# #!/bin/bash -e
+#
+# sudo apt-get install -qq -y wget ca-certificates
+#
+# echo "================= Installing Postgre 9.6.3 ==================="
+# sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+# wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+# sudo apt-get update -qq
+# sudo apt-get install -qq -y postgresql-9.6 postgresql-server-dev-9.6
+# sudo apt-get install -qq postgis
+#
+# # Fix bug https://github.com/docker/docker/issues/783
+# # which prevents postgresql from staring when using aufs
+# # This workaround proposed in the comment to the issue: https://github.com/docker/docker/issues/783#issuecomment-56013588
+# # I've test this on my own builds
+# mkdir /etc/ssl/private-copy
+# mv /etc/ssl/private/* /etc/ssl/private-copy/
+# rm -r /etc/ssl/private
+# mv /etc/ssl/private-copy /etc/ssl/private
+# chmod -R 0700 /etc/ssl/private
+# chown -R postgres /etc/ssl/private
